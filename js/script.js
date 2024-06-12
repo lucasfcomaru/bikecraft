97f24215471731f90c97e2058ac5d3e04131b255
lucasfcomaru <lukscomaru@gmail.com>
2024-06-12 15:18:17 -0300

    perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar links do menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -11,3 +12,36 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+const parametros = new URLSearchParams(window.location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.querySelector("#" + parametro);
+    if (elemento) {
+        elemento.checked = true;
+    }
+}
+
+parametros.forEach(ativarProduto);
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+console.log(perguntas);
