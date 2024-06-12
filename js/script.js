a69fe9d0815077b0d1cdf17fac3ba7346ab4d5e2
lucasfcomaru <lukscomaru@gmail.com>
2024-06-12 19:45:09 -0300

    plugin de animação

--- js/script.js
+++ js/script.js
@@ -44,4 +44,30 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
-console.log(perguntas);
+
+// Galeria de imagens
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia("(min-width: 935px)").matches;
+
+    if (media) {
+        galeriaContainer.prepend(img);
+    }
+}
+
+function eventosGaleria(img) {
+    img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// Animação
+if (window.SimpleAnime) {
+    new SimpleAnime();
+}
+
+
+// console.log();
